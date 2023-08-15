6cb83bfa794e3c7704331d26364a8eb21e5eb042
bjjinlaw <123348980+bjjinlaw@users.noreply.github.com>
2023-08-15 15:49:24 +0545

    Update urls.py

--- testapp/urls.py
+++ testapp/urls.py
@@ -1,12 +1,9 @@
 from rest_framework import routers
-from testapp.views import TaskViewSet, CategoryViewSet, Home
+from testapp.views import Home
 from django.urls import path, include
 
 
 router = routers.SimpleRouter()
-router.register("tasks", TaskViewSet, basename="task")
-
-router.register("categories", CategoryViewSet, basename="category")
 
 urlpatterns = [
     path("", Home.as_view(), name="home"),
